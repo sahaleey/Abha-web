88f3cf8226ee39e24b11a79b438f9782d5f1c4dd
crazydrace <ajua46244@gmail.com>
2025-04-29 17:47:46 +0530

    why always errors

--- abha-web/src/pages/AdminUpload.jsx
+++ abha-web/src/pages/AdminUpload.jsx
@@ -45,7 +45,7 @@ const AdminUpload = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/programmes",
+        "https://abha-web-1.onrender.com/api/programmes",
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
       );
@@ -92,7 +92,7 @@ const AdminUpload = () => {
   const handleDelete = async (id) => {
     try {
       const response = await axios.delete(
-        `http://localhost:5000/api/programmes/${id}`
+        `https://abha-web-1.onrender.com/api/programmes/${id}`
       );
       if (response.status === 200) {
         setUploadedProgrammes(
@@ -205,7 +205,7 @@ const AdminUpload = () => {
             >
               {prog.image && (
                 <img
-                  src={`http://localhost:5000/uploads/${prog.image}`}
+                  src={`https://abha-web-1.onrender.com/uploads/${prog.image}`}
                   alt={prog.name}
                   className="w-full h-60 object-cover transition-transform duration-500 ease-out transform hover:scale-110"
                 />
