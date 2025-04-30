57eeadc67ba41e04674a7bea00819fd747bfaec6
crazydrace <ajua46244@gmail.com>
2025-04-30 12:10:56 +0530

    added

--- abha-web/src/components/Footer.jsx
+++ abha-web/src/components/Footer.jsx
@@ -7,9 +7,12 @@ const Footer = () => {
       <div className="max-w-6xl mx-auto px-6 py-16 grid grid-cols-1 md:grid-cols-3 gap-10">
         {/* About Abha */}
         <div>
-          <h3 className="text-2xl font-bold text-amber-400 mb-4 font-caudex">Abha</h3>
+          <h3 className="text-2xl font-bold text-amber-400 mb-4 font-caudex">
+            Abha
+          </h3>
           <p className="text-sm text-gray-300 leading-relaxed">
-            Empowering creativity, passion, and purpose. Join our vibrant community and discover programs that inspire.
+            Empowering creativity, passion, and purpose. Join our vibrant
+            community and discover programs that inspire.
           </p>
         </div>
 
@@ -17,10 +20,29 @@ const Footer = () => {
         <div className="">
           <h4 className="text-xl font-semibold text-white mb-4">Quick Links</h4>
           <ul className="space-y-2 text-gray-300 gap-5 flex">
-            <li><a href="/" className="hover:text-amber-400 transition">Home</a></li>
-            <li><a href="/about" className="hover:text-amber-400 transition">About</a></li>
-            <li><a href="/blog" className="hover:text-amber-400 transition">Blog</a></li>
-            <li><a href="/programmes" className="hover:text-amber-400 transition ">Programs</a></li>
+            <li>
+              <a href="/" className="hover:text-amber-400 transition">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="hover:text-amber-400 transition">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/blog" className="hover:text-amber-400 transition">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a
+                href="/programmes"
+                className="hover:text-amber-400 transition "
+              >
+                Programs
+              </a>
+            </li>
           </ul>
         </div>
         {/* import { AiFillLike, AiFillDislike  } from "react-icons/ai";
@@ -30,7 +52,10 @@ import { GrFormView } from "react-icons/gr"; */}
         <div>
           <h4 className="text-xl font-semibold text-white mb-4">Follow Us</h4>
           <div className="flex space-x-4 text-lg">
-            <a href="#" className="text-blue-400 hover:text-white transition">
+            <a
+              href="https://www.instagram.com/_abha_nric/"
+              className="text-blue-400 hover:text-white transition"
+            >
               <FaInstagram />
             </a>
             <a href="#" className="text-blue-400 hover:text-white transition">
